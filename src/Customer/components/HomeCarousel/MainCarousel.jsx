c742b42d2b1ff1edaf5503307c1dd2b414169fa4
nybzmr <nybzmr02@gmail.com>
2024-04-28 01:03:57 +0530

    Added the carousel

--- src/Customer/components/HomeCarousel/MainCarousel.jsx
+++ src/Customer/components/HomeCarousel/MainCarousel.jsx
@@ -1,29 +1,21 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { mainCaraouselData } from "./mainCarouselData";
 
-const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
-];
-
-const MainCarousel = () => (
+const MainCarousel = () => {
+  const items = mainCaraouselData.map((item)=> <img className = 'cursor-pointer' role = 'presentation' src= {item.image} alt =''/>);
+  return (
     <AliceCarousel
-        autoPlay
-        autoPlayControls
-        autoPlayStrategy="none"
-        autoPlayInterval={1000}
-        animationDuration={1000}
-        animationType="fadeout"
-        infinite
-        touchTracking={false}
-        disableDotsControls
-        disableButtonsControls
-        items={items}
+      mouseTracking
+      items={items}
+     controlsStrategy="alternate"
+     disableButtonsControls
+     autoPlay
+     autoPlayInterval={1000}
+     infinite
     />
-);
+  );
+};
 
 export default MainCarousel;
