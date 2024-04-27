1bcc800bee438e7124745be610d25f85522c4608
nybzmr <nybzmr02@gmail.com>
2024-04-27 23:59:30 +0530

    Initialized repo, Added mui, tailwind, Navbar And MainCarousel.

---
+++ src/Customer/components/HomeCarousel/MainCarousel.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+const items = [
+    <div className="item" data-value="1">1</div>,
+    <div className="item" data-value="2">2</div>,
+    <div className="item" data-value="3">3</div>,
+    <div className="item" data-value="4">4</div>,
+    <div className="item" data-value="5">5</div>,
+];
+
+const MainCarousel = () => (
+    <AliceCarousel
+        autoPlay
+        autoPlayControls
+        autoPlayStrategy="none"
+        autoPlayInterval={1000}
+        animationDuration={1000}
+        animationType="fadeout"
+        infinite
+        touchTracking={false}
+        disableDotsControls
+        disableButtonsControls
+        items={items}
+    />
+);
+
+export default MainCarousel;
---
+++ src/Customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+
+const HomePage = () => {
+  return (
+    <>
+        <MainCarousel/>
+        <div>
+            Other section
+        </div>
+    </>
+  )
+}
+
+export default HomePage
