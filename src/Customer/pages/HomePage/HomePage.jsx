dec75f2fb00ce6405fea9f366b81990ec757fb98
nybzmr <nybzmr02@gmail.com>
2024-04-28 20:43:25 +0530

    Added home page carousel

--- src/Customer/pages/HomePage/HomePage.jsx
+++ src/Customer/pages/HomePage/HomePage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+import HomeSectionCarousel from '../../components/HomeSectionCarousel/HomeSectionCarousel'
+import { mens_kurta } from '../../../Data/mens_kurta'
 
 const HomePage = () => {
   return (
     <>
         <MainCarousel/>
-        <div>
-            Other section
+        <div className='space-y-10 py-20 '>
+            <HomeSectionCarousel data={mens_kurta}/>
+            <HomeSectionCarousel data={mens_kurta}/>
+            <HomeSectionCarousel data={mens_kurta}/>
+            <HomeSectionCarousel data={mens_kurta}/>
+            <HomeSectionCarousel data={mens_kurta}/>
+            <HomeSectionCarousel data={mens_kurta}/>
         </div>
     </>
   )
